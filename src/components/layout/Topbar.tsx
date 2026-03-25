b1858ae710465455084485e08d25d6190a17f1d9
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-25 19:50:02 +0900

    chore: 올바른 파일 경로로 수정

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -27,7 +27,7 @@ export default function Topbar() {
           공유
         </button>
         <a
-          href="/public/이서림_포트폴리오.pdf"
+          href="/이서림_포트폴리오.pdf"
           download
           className={clsx(baseBtn, "bg-accent text-white hover:opacity-85")}
         >
